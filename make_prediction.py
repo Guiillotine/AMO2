db75ca6992bfef71ac3f577554e0edcd80a11e71
e.kozlova <e.kozlova@estesis.tech>
2025-03-24 17:24:02 +0700

    added model evaluation

--- make_prediction.py
+++ make_prediction.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import pickle
+from sklearn.metrics import classification_report
+
+NUM = 5
 
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
@@ -14,7 +17,14 @@ X_test = data_test.drop('cut', axis=1)
 # Выполнение предсказаний
 y_pred = model.predict(X_test)
 
-for i in range(25, 30):
+print(f"Печать {NUM} предсказаний:")
+for i in range(NUM):
     predicted_val = y_pred[i][0]
     true_val = y_test[i]
-    print(f"fact: {predicted_val} | predicted: {true_val} | delta: {true_val-predicted_val}")
+    print(
+        f"fact: {target_numbers_vals[predicted_val]:<10} | pred: {target_numbers_vals[true_val]:<10} | delta: {true_val-predicted_val}"
+    )
+
+print("\nОценка модели:")
+print(classification_report(y_test, y_pred, zero_division=0))
+print("score =", round(model.score(X_test,y_test), 2))
